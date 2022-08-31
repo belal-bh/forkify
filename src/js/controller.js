fd3fb9ca72dbc976a92dfb3e5016dfda7a28de61
Md. Belal Hossain <belal.cseai@gmail.com>
2022-08-31 20:19:07 +0600

    implements search results

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,19 +1,16 @@
 import 'core-js/stable'; // Polyfiling async await
+import { async } from 'regenerator-runtime';
 import 'regenerator-runtime/runtime'; // Polyfiling others
 
 import * as model from './model';
 import reciepeView from './views/reciepeView';
+import searchView from './views/searchView';
 
 const recipeContainer = document.querySelector('.recipe');
-
-// https://forkify-api.herokuapp.com/v2
-
-///////////////////////////////////////
 
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
-    console.log(id);
 
     if (!id) return;
     reciepeView.renderSpinner();
@@ -22,15 +19,32 @@ const controlRecipes = async function () {
     await model.loadRecipe(id);
     const { recipe } = model.state;
 
-    // Rendering recipe
+    // 2) Rendering recipe
     reciepeView.render(model.state.recipe);
   } catch (err) {
     reciepeView.renderError();
   }
 };
 
+const controlSearchResults = async function () {
+  try {
+    // 1) Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // 2) Load search results
+    await model.loadSearchResults(query);
+
+    // 3) Render results
+    console.log(model.state.search.results);
+  } catch (err) {
+    console.err(`${err} 💥💥💥`);
+  }
+};
+
 const init = function () {
   reciepeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 };
 
 init();
